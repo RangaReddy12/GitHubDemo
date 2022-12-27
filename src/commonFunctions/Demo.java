c7c373a2041e088eba073b7f99e00ee2af618f30
MAX HUB <MAX HUB@DESKTOP-4M14A0P>
2022-12-27 09:09:45 +0530

    New Methods Added

---
+++ src/commonFunctions/Demo.java
@@ -0,0 +1,23 @@
+package commonFunctions;
+
+public class Demo {
+public void add()
+{
+	int a=45,b=89,c;
+	c=a+b;
+	System.out.println(c);
+}
+public void mul()
+{
+	int a=45,b=89,c;
+	c=a*b;
+	System.out.println(c);
+}
+public void sub()
+{
+	int a=45,b=89,c;
+	c=a-b;
+	System.out.println(c);
+}
+
+}
